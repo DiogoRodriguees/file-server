e63a80aedf9bfb730c8adc46b50e7f4120e68525
diogorodriguees <diogorodrigueslife@gmail.com>
2024-06-24 21:32:31 -0300

    :sparkles: add app

--- server/app/app.go
+++ server/app/app.go
@@ -1,11 +1,20 @@
 package app
 
 import (
+	"log"
+	"os"
 	"server/socket"
 )
 
 func Run() {
 	listener := socket.CreateListener()
 	defer listener.Close()
+
+	err := os.MkdirAll("uploads", 0755)
+	if err != nil {
+		log.Fatal("Error os make dir uploads: ", err)
+		os.Exit(1)
+	}
+
 	socket.ListenerConnections(listener)
 }
